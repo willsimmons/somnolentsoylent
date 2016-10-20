c6e68f3214341c177cb9903107cd18ac1d5c23e5
Omar Mohamed <omar@omohamed.com>
2016-10-19 19:24:23 -0700

    create Event schema

---
+++ server/schemas/eventsSchema.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var eventSchema = new Schema({
+  id: Schema.Types.ObjectId,
+  name: String,
+  location: {
+    type: [Number],
+    index: '2dsphere'
+  }
+  startTime: Date,
+  endTime: Date,
+  tags: [String],
+  invitedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  checkedInUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  visibility: String,
+  savedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+})
+
+var Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
