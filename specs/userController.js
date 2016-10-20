497a49661ec17bd313f841607c9777398d8aa8b9
lopez567 <spencer.r.lopez@gmail.com>
2016-10-20 12:31:43 -0700

    User spec bug fix

--- specs/userController.js
+++ specs/userController.js
@@ -3,13 +3,28 @@ var expect = chai.expect;
 var userController = require('../server/controllers/userController');
 var mongoose = require('mongoose');
 
+var testUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@test.com',
+  password: 'password'
+}
+
 describe('User Controller', function() {
   describe('signUp', function() {
-    it('should add a new user to the database', function() {
-      expect(true).to.equal(true);
+    it('should add a new user to the database', function(done) {
+      request(app)
+          .post('/api/users/signup')
+          .send(testUser)
+          .expect(301)
+          .end(done);
     });
-    it('should return TODOBLANKBLANK when email already exists', function() {
-      expect(true).to.equal(true);
+    it('should return a 400 when email already exists', function(done) {
+      request(app)
+          .post('/api/users/signup')
+          .send(testUser)
+          .expect(400)
+          .end(done);
     });
   });
 });
