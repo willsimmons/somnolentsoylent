5cd9c9d80d527bcff7e207bc52cee7815038db4a
Omar Mohamed <omar@omohamed.com>
2016-10-20 15:20:19 -0700

    create default endTime that is 6 hours after the startTime, only if the endTime is not specified on an Event add test to ensure Eventschema is not changed

--- server/schemas/eventsSchema.js
+++ server/schemas/eventsSchema.js
@@ -1,19 +1,34 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var eventSchema = new Schema({
-  id: Schema.Types.ObjectId,
+  id: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
   name: String,
   location: {
     type: [Number],
-    index: '2dsphere'
+    index: '2dsphere',
+    required: true
   },
-  startTime: Date,
+  startTime: {
+    type: Date,
+    default: Date.now
+  },
   endTime: Date,
   tags: [String],
   invitedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   checkedInUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  visibility: String,
+  visibility: {
+    type: String,
+    default: 'Public'
+  },
   savedUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+})
+
+eventSchema.pre('save', function (next) {
+  this.endTime = this.endTime || (this.startTime.getDate() + .25);
+  next();
 })
 
 var Event = mongoose.model('Event', eventSchema);
