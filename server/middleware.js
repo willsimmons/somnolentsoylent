f84bcca9596db1fe97e361066aaa99a473ef2c96
Omar Mohamed <omar@omohamed.com>
2016-10-26 14:39:43 -0700

    Update topBar component delete client folder update server middleware

--- server/middleware.js
+++ server/middleware.js
@@ -5,6 +5,4 @@ module.exports = function (app, express) {
   // Parse Post Bodys
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  // Host Static Files
-  app.use(express.static(__dirname + '/../../client'));
 };
