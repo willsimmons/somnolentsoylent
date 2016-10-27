81abe7f1b9ef56f924d4fac1064d7786dbcd2ecf
lopez567 <spencer.r.lopez@gmail.com>
2016-10-27 15:36:54 -0700

    Update event card

--- Sembly/app/components/EventCard.js
+++ Sembly/app/components/EventCard.js
@@ -8,23 +8,6 @@ import {
   View
 } from 'react-native';
 
-var testEvent = { __v: 0,
-  endTime: new Date('Thu Oct 27 2016 20:13:44 GMT-0700 (PDT)'),
-  name: 'Basketball',
-  location: [ -122.4066, 37.7786 ],
-  image: 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Basketball.png',
-  _id: '58126e082dd1b517c1122674',
-  savedUsers: [],
-  visibility: 'public',
-  checkedInUsers: [],
-  invitedUsers:
-   ['58126e052dd1b517c1122670',
-     '58126e052dd1b517c1122671',
-     '58126e052dd1b517c1122672',
-     '58126e052dd1b517c1122673'],
-  tags: [ 'sports', 'basketball' ],
-  startTime: new Date('Thu Oct 27 2016 14:13:44 GMT-0700 (PDT)')};
-
 export default class EventCard extends Component {
   constructor(props) {
     super(props);
