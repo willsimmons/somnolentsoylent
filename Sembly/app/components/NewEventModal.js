1d74c086df31cfb7d2c6a56156541177620b9430
Omar Mohamed <omar@omohamed.com>
2016-10-28 14:55:39 -0700

    further updates to newEventModal

--- Sembly/app/components/NewEventModal.js
+++ Sembly/app/components/NewEventModal.js
@@ -4,7 +4,9 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  ScrollView,
+  TextInput
 } from 'react-native';
 
 import Modal from 'react-native-modalbox';
@@ -18,11 +20,27 @@ export default class NewEventModal extends Component {
     let context = this;
     return (
       <Modal ref={'newEventModal'} style={styles.modal} isOpen={this.props.visibility}>
-        <View style={styles.container}>
-          <Text>Create a New Event!</Text>
-          <TouchableOpacity onPress={() => context.refs.newEventModal.close()}>
+        <View>
+          <View style={styles.closeButtonContainer}>
+            <TouchableOpacity onPress={() => context.refs.newEventModal.close()}>
             <Icon style={styles.closeButton} name='close'/>
-          </TouchableOpacity>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.headerContainer}>
+            <Text style={styles.header}>Create a New Event!</Text>
+          </View>
+          <View style={styles.titleInputContainer}>
+            <TextInput
+              style={styles.titleInput}
+              placeholder='Enter a title for your Event'/>
+          </View>
+          //Add location
+          //Choose Tags
+          //handleImage
+          //Start Time Picker
+          //End Time Picker
+          //Invite Friends
+          //Set Visibility
         </View>
       </Modal>
     )
@@ -34,11 +52,35 @@ const styles = StyleSheet.create({
   modal: {
     marginTop: 40
   },
-  container: {
+  closeButtonContainer: {
     flexDirection: 'row',
-    justifyContent: 'center'
+    justifyContent: 'flex-end'
   },
   closeButton:{
     fontSize: 30
+  },
+  headerContainer:{
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  titleInputContainer: {
+    // flexDirection: 'row',
+    // justifyContent: 'center',
+    // alignSelf: 'stretch'
+  },
+  titleInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 8,
+    textAlign: 'center'
   }
 })
